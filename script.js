5b0f178e755e96e7aec18bf4fab1d6fd03d04b8c
VKozhura <kozhura_v@mail.ru>
2022-01-13 17:29:19 +0300

    добавила проверку соответсвия паролей

--- script.js
+++ script.js
@@ -19,10 +19,14 @@ const showSuccess = (input) => {
 };
 
 //Check email is valid
-const isValidEmail = (email) => {
-	return email.match(
-		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-	);
+const checkEmail = (input) => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (re.test(input.value)) {
+		showSuccess(input);
+	} else {
+		showError(input, "Email is not valid");
+	}
 };
 
 //check input length
@@ -33,6 +37,13 @@ const checkLength = (input, min, max) => {
 		showError(input, `${getFieldName(input)} must be less than ${max} characters`);
 	} else {
 		showSuccess(input);
+	}
+};
+
+//check passwords match
+const checkPasswordsMatch = (input1, input2) => {
+	if (input1.value !== input2.value) {
+		showError(input2, "Passwords do not match");
 	}
 };
 
@@ -59,4 +70,6 @@ form.addEventListener("submit", (e) => {
 	checkRequired([username, email, password, password2]);
 	checkLength(username, 3, 15);
 	checkLength(password, 6, 25);
+	checkEmail(email);
+	checkPasswordsMatch(password, password2);
 });
